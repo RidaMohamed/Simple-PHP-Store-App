6ff4c23cefcdfc08526a8753c42b49f6e71f7cc2
Salhi <mohamed-elridha.salhi7@etu.univ-lorraine.fr>
2018-11-17 23:16:38 +0100

    depot 2 salhi

--- aliment.php
+++ aliment.php
@@ -55,6 +55,28 @@ foreach ($Recettes as $key => $value) {
     
 }*/
 
+
+    echo '<form>';
+
+    for($j ; $j<8 ; $j++){ 
+        
+        echo '<button id="'.$j.'" type="button" onclick="echho(this.id)" >';
+        echo $Hierarchie['Aliment']['sous-categorie'][$j];
+        echo '</button>';
+        echo '<br>';
+}
+ 
+   echo '</form>';
+   $id = 0 ;
+   
+   
+     function writeMessage() {
+
+         echo 'Fruit';
+         echo $Hierarchie['Aliment']['sous-categorie'][0];
+            }
+   
+   
 ?>
 
 
@@ -62,32 +84,179 @@ foreach ($Recettes as $key => $value) {
     <head>
         <meta charset="UTF-8">
         <title></title>
-    </head>
-    <body>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script>
-        
-        function echho (){
-            var x = document.getElementById("H").innerHTML ; 
+            
+         function echho (id){
+          
+             
+            /*var x = document.getElementById("H").innerHTML ; 
             x = x + "<br> Recette";
             document.getElementById("H").innerHTML = x;
-        }
+            alert(id);
+            var t = "dsds";
+            document.getElementById("H2").innerHTML = t;*/
+            
+          }
         </script>
+    </head>
+    <body>
  
-        <?php
-        echo '<form>';
-
-for($j ; $j<8 ; $j++){
-    echo '<button type="button" onclick="echho()" >';
-    echo $Hierarchie['Aliment']['sous-categorie'][$j].'<br>';
-    echo '</button>';
-    echo '<br>';
-}
-echo '</form>';
-        ?>
+ 
+       
         <p id="H">
-            ddfdfdfdfdf
+            <?php
+            $sousCatg= array();
+            $k = 0 ;
+            foreach ($Hierarchie as $key => $value) {
+                if($key == 'Fruit'){
+                foreach ($value as $key1 => $value1) {
+                        if ($key1 == 'sous-categorie') {
+                        foreach ($value1 as $key2 => $value2) {
+                            echo '<button type="button" onclick="echho(this.id)" >';
+                            array_push($sousCatg, $value2);
+                            
+                            echo $value2;
+                            echo '</button>';
+                            echo '<br>';
+                            $k ++ ;
+                            
+                        }
+                    }
+                    
+                }
+               }
+            }
+            
+           
+            
+            ?>
         </p>
-       echo <button type="button" onclick="echho()"> gfhfghfghfgh</button>
+        
+        <form id="H2">
+            
+            <?php
+           $sousCatg1 = array();
+             for ($index = 0; $index < count($sousCatg); $index++) {
+                foreach ($Hierarchie as $key => $value) {
+                    if ($key == $sousCatg[$index]) {
+                       foreach ($value as $key1 => $value1) {
+                         if ($key1 == 'sous-categorie') {
+                           foreach ($value1 as $key2 => $value2) {
+                            echo '<button type="button" onclick="echho(this.id)" >';
+                            //array_push($sousCatg, $k , $value2);
+                            echo $value2;
+                            array_push($sousCatg1, $value2);
+                            echo '</button>';
+                            echo '<br>';
+                            $k ++ ;
+                            
+                        }
+                    }
+                    } 
+                    }
+                    
+                    
+                }
+                
+            }
+            
+            ?>
+        </form>
+        
+        <form id="H2">
+            
+            <?php
+            echo '<br>';echo '<br>';echo '<br>';echo '<br>';
+          $sousCatg2 = array();
+             for ($index = 0; $index < count($sousCatg1); $index++) {
+                foreach ($Hierarchie as $key => $value) {
+                    if ($key == $sousCatg1[$index]) {
+                       foreach ($value as $key1 => $value1) {
+                         if ($key1 == 'sous-categorie') {
+                           foreach ($value1 as $key2 => $value2) {
+                            echo '<button type="button" onclick="echho(this.id)" >';
+                            //array_push($sousCatg, $k , $value2);
+                            echo $value2;
+                            array_push($sousCatg2, $value2);
+                            echo '</button>';
+                            echo '<br>';
+                            $k ++ ;
+                            
+                        }
+                    }
+                    } 
+                    }
+                    
+                    
+                }
+                
+            }
+            
+            ?>
+        </form>
+        
+         <form id="H2">
+            
+            <?php
+            echo '<br>';echo '<br>';echo '<br>';echo '<br>';
+          $sousCatg3 = array();
+             for ($index = 0; $index < count($sousCatg2); $index++) {
+                foreach ($Hierarchie as $key => $value) {
+                    if ($key == $sousCatg2[$index]) {
+                       foreach ($value as $key1 => $value1) {
+                         if ($key1 == 'sous-categorie') {
+                           foreach ($value1 as $key2 => $value2) {
+                            echo '<button type="button" onclick="echho(this.id)" >';
+                            //array_push($sousCatg, $k , $value2);
+                            echo $value2;
+                            array_push($sousCatg3, $value2);
+                            echo '</button>';
+                            echo '<br>';
+                            $k ++ ;
+                            
+                        }
+                    }
+                    } 
+                    }
+                    
+                    
+                }
+                
+            }
+            
+            ?>
+        </form>
+        
+        <form id="H2">
+            
+            <?php
+            echo '<br>';echo '<br>';echo '<br>';echo '<br>';
+            $titre ;
+            foreach ($Recettes as $key => $value) {
+                foreach ($value as $key1 => $value1) {
+                    if($key1 == 'titre')
+                        $titre = $value1;
+                
+                if ($key1 == 'index') {
+                    foreach ($value1 as $key2 => $value2) {
+                        for ($index1 = 0; $index1 < count($sousCatg1); $index1++) {
+                            if($value2 == $sousCatg1[$index1]){
+                                echo $titre;
+                                echo '<br>';
+                            }
+                        }
+                    }
+                    
+                }
+    
+                }
+            }
+            
+            ?>
+        </form>
+             
+        <button id="hahah" type="button" > gfhfghfghfgh</button>
      
     </body>
 </html>
